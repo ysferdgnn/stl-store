d5eec380ac78f864524c9945e85fc4bbb4951c25
ysferdgnn <ysferdgnn@users.noreply.github.com>
2023-11-18 00:53:25 +0300

    add .env

--- next.config.js
+++ next.config.js
@@ -3,13 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    API_KEY:'',
-AUTH_DOMAIN:'',
-PROJECT_ID:'',
-STORERAGE_BUCKET:'',
-MESSAGING_SENDER:'',
-APP_ID:'',
-MEASUREMENT_ID: '',
+    API_KEY:'AIzaSyDsKrmDcrJcAD06GLPTy103O59xWw8ti88',
+AUTH_DOMAIN:'stl-store-d1df0.firebaseapp.com',
+PROJECT_ID:'stl-store-d1df0',
+STORERAGE_BUCKET:'stl-store-d1df0.appspot.com',
+MESSAGING_SENDER:'570762115321',
+APP_ID:'1:570762115321:web:4c7b507d0170fe69c34a28',
+MEASUREMENT_ID:'G-VJ9C7RVX9X',
   },
 }
 
